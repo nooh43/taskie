c8808604d5be3c03afc6d3d496bfe3a478aeb214
NasserHumood <nooh43@live.com>
2018-10-04 01:21:14 -0600

    Adding Welcome Message

--- taskie.sh
+++ taskie.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 # Taskie : Task documentation app
-# Version : V 0.0.0
+# Version : V 0.0.1
 # Author  : Nasser Alhumood
 # .-.    . . .-.-.
 # |.|.-.-|-.-|-`-..-,.-.. .
 # `-``-`-'-' ' `-'`'-'   `
+clear
+
+# Some Unnecessary Variables, but they're here anyway
+version=V0.0.1
+
+# Welcome Massage
+echo -e "\e[1;34;1m+=====================================================\e[0m"
+echo -e "\e[1;34;1m+\e[0m" "Taskie -  " $version
+echo -e "\e[1;34;1m+\e[0m" "Task Documentation and notification with status"
+echo -e "\e[1;34;1m+=====================================================\e[0m"
+echo
+echo
+
+# Secondary Welcome Message
+echo -e "Welcome to Taskie, we will work together to create your tasks and setting them up correctly."
